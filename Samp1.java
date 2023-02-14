9ad2a0f5661868251fe87ead25d16c1bbc63d022
Soundarya-B-N <sbns71070@gmail.com>
2023-02-15 00:06:24 +0530

    push

---
+++ Samp1.java
@@ -0,0 +1,18 @@
+class Demo2
+{
+ static double area()
+ {
+  int b=2;
+  int h=3;
+  double y=0.5*b*h;
+  return y;
+  }
+ }
+ class Samp1
+ {
+  public static void main(String[]args)
+  {
+   double x=area();
+   System.out.println(x);
+   }
+  }
